7b0122e770c1bc0ef75af76fdde83757030e4025
pedro-almeida97 <pedro.almeidah@ufu.br>
2025-09-04 11:47:36 -0300

    carregando css

--- webpack.config.js
+++ webpack.config.js
@@ -27,4 +27,13 @@ module.exports = {
       favicon: path.resolve('src', 'assets', 'scissors.svg'),
     }),
   ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 }
