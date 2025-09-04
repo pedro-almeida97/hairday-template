451e689fdb4c9f77f72b14db9d055cfc09b8f124
pedro-almeida97 <pedro.almeidah@ufu.br>
2025-09-04 11:35:31 -0300

    carregando html

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  target: 'web',
+  mode: 'development',
+
+  entry: path.resolve(__dirname, 'src', 'main.js'),
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true, // limpa a pasta dist a cada build
+  },
+
+  devServer: {
+    port: 3000,
+    open: true,
+    liveReload: true,
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'index.html'),
+    }),
+  ],
+}
