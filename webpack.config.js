e549e542227e3b0d43816392cb6f9e0d71637fd7
pedro-almeida97 <pedro.almeidah@ufu.br>
2025-09-04 12:03:27 -0300

    copiando assets

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   target: 'web',
@@ -9,7 +10,7 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true, // limpa a pasta dist a cada build
+    clean: true,
   },
 
   devServer: {
@@ -25,6 +26,14 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'index.html'),
       favicon: path.resolve('src', 'assets', 'scissors.svg'),
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src', 'assets'),
+          to: path.resolve(__dirname, 'dist', 'src', 'assets'),
+        },
+      ],
     }),
   ],
 
