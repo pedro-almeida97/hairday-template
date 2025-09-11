b9756fb75b1fbadab232d5b3c018f0a2f4615634
pedro-almeida97 <pedro.almeidah@ufu.br>
2025-09-10 21:55:01 -0300

    data atual

--- src/main.js
+++ src/main.js
@@ -5,3 +5,5 @@ import './libs/dayjs'
 import './styles/global.css'
 import './styles/form.css'
 import './styles/schedule.css'
+
+import './modules/form/submit.js'
---
+++ src/modules/form/submit.js
@@ -0,0 +1,15 @@
+import dayjs from 'dayjs'
+
+const form = document.querySelector('form')
+const selectedDate = document.getElementById('date')
+
+const inputToday = dayjs(new Date()).format('YYYY-MM-DD')
+
+selectedDate.value = inputToday
+selectedDate.min = inputToday
+
+form.onsubmit = (e) => {
+  e.preventDefault()
+
+  console.log('ENVIADO')
+}
