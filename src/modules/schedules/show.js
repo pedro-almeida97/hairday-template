babcfe209ca806b5dcbb9df4c2492e6bf6eb71ec
pedro-almeida97 <pedro.almeidah@ufu.br>
2025-10-01 19:42:35 -0300

    inicio renderizando horarios

---
+++ src/modules/schedules/show.js
@@ -0,0 +1,7 @@
+import dayjs from 'dayjs'
+
+const periodMorning = document.getElementById('period-morning')
+const periodAfeternoon = document.getElementById('period-afternoon')
+const periodNight = document.getElementById('period-night')
+
+export function schedulesShow({ dailySchedules }) {}
