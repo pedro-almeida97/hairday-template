505e3e43196838beebedd337675a11322403db52
pedro-almeida97 <pedro.almeidah@ufu.br>
2025-10-06 21:58:40 -0300

    finalizando r renderizando os agendamentos do dia

--- src/modules/schedules/show.js
+++ src/modules/schedules/show.js
@@ -4,4 +4,41 @@ const periodMorning = document.getElementById('period-morning')
 const periodAfeternoon = document.getElementById('period-afternoon')
 const periodNight = document.getElementById('period-night')
 
-export function schedulesShow({ dailySchedules }) {}
+export function schedulesShow({ dailySchedules }) {
+  try {
+    periodMorning.innerHTML = ''
+    periodAfeternoon.innerHTML = ''
+    periodNight.innerHTML = ''
+
+    dailySchedules.array.forEach((schedule) => {
+      const item = document.createElement('li')
+      const time = document.createElement('strong')
+      const name = document.createElement('span')
+
+      item.setAttribute('data-id', schedule.id)
+
+      time.textContent = dayjs(schedule.when).format('HH:mm')
+      name.textContent = schedule.name
+
+      const cancelIcon = document.createElement('img')
+      cancelIcon.classList.add('cancel-icon')
+      cancelIcon.setAttribute('src', './src/assets/cancel.svg')
+      cancelIcon.setAttribute('alt', 'Cancelar')
+
+      item.append(time, name, cancelIcon)
+
+      const hour = dayjs(schedule.when).hour()
+
+      if (hour <= 12) {
+        periodMorning.appendChild(item)
+      } else if (hour > 12 && hour <= 18) {
+        periodAfeternoon.appendChild(item)
+      } else {
+        periodNight.appendChild(item)
+      }
+    })
+  } catch (error) {
+    alert('Não foi possível exibir os agendamentos')
+    console.log(error)
+  }
+}
