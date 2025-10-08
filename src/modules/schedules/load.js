ab03b99e47f364631e7c5183f7a7c8ee8d05654c
pedro-almeida97 <pedro.almeidah@ufu.br>
2025-10-07 21:40:02 -0300

    correções

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,11 +1,14 @@
 import { scheduleFetchByDay } from '../../services/schedule-fetch-by-day'
 import { hoursLoad } from '../form/hours-load'
+import { schedulesShow } from './show'
 
 const selectedDate = document.getElementById('date')
 
 export async function schedulesDay() {
   const date = selectedDate.value
-  const hours = hoursLoad({ date })
+  const hours = hoursLoad({ date, dailySchedules })
+
+  schedulesShow({ dailySchedules })
 
   const dailySchedules = await scheduleFetchByDay({ date })
   console.log(dailySchedules)
